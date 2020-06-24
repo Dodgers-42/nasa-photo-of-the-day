a242591ede558004a5ecbc84bc2d11d1c4619fef
Dodger-42 <kevin.bing.barber@gmail.com>
2020-06-24 16:18:48 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
 import "./App.css";
 import NASA from "./NASA/NASA";
+// import NasaPhoto from "./NasaPhoto/NasaPhoto"
 
 function App() {
+  React.useEffect(() => {
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=lEfomzhjJqJm96qWclfe9yfNtGf4dfeVuTUfb6G2"
+    )
+      .then(res => console.log(res))
+        .catch(err => console.log)
+
+    
+  }, []);
   return (
     <div className="App">
       <p>
--- src/NASA/NASA.js
+++ src/NASA/NASA.js
@@ -1,31 +1,17 @@
 import React, { useEffect } from "react";
-// import axios from "axios";
 import "./NASA.css";
 import axios from "axios";
 
 const NASA = () => {
-    const [nasaPhoto, setNasaPhoto] = useState({});
-    useEffect(() => {
-        axios
-            .get(
-                "https://api.nasa.gov/planetary/apod?api_key=lEfomzhjJqJm96qWclfe9yfNtGf4dfeVuTUfb6G2"
-            )
-            .then((response) => {
-                console.log("Response object", response);
-                setNasaPhoto(response.data);
-            })
-            .catch((error) => {
-                console.log("Error object", error);
-            });
-    }, []);
-    console.log("Here", nasaPhoto);
-    
+    // const [nasaPhoto, setNasaPhoto] = useState({});
+    // useEffect(() => {}
+        
     return (
       <div className="photo">
-        {nasaPhoto.map((photo) => {
+        {/* {nasaPhoto.map((photo) => {
             <NASA key={photo.copyright} photo={photo} />;
         })}
-         
+          */}
       </div>
     );
   }
