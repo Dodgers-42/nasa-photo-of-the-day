62bbad865349a027d0a6a6ce8a042f5f47253f20
Dodger-42 <kevin.bing.barber@gmail.com>
2020-06-25 13:36:23 -0700

    first

--- src/NASA/NASA.js
+++ src/NASA/NASA.js
@@ -1,19 +1,13 @@
 import React, { useEffect } from "react";
 import NasaPhoto from "./NasaPhoto";
-// import "./Nasa.css";
-// import axios from "./node_modules/axios";
 
 const Nasa = (props) => {
-    // const [nasaPhoto, setNasaPhoto] = useState({});
-    // useEffect(() => {}
+    
         console.log(props.nasaInfo);
     return (
       <div className="nasa">
-        {/* {nasaPhoto.map((photo) => {
-            <Nasa key={photo.copyright} photo={photo} />;
-        })}
-          */}
-          <NasaPhoto nasaPhoto = {props.nasaInfo.url} title = {props.nasaInfo.title}/>
+        
+          <NasaPhoto nasaPhoto = {props.nasaInfo.url} title = {props.nasaInfo.title} copyright = {props.nasaInfo.copyright} date = {props.nasaInfo.date} explanation = {props.nasaInfo.explanation}/>
       </div>
     );
   }
--- src/NASA/NasaPhoto.js
+++ src/NASA/NasaPhoto.js
@@ -7,7 +7,10 @@ const NasaPhoto = (props) => {
     return (
       <div className="nasaphoto">
         <img src={props.nasaPhoto}/>
+        <header>{props.date}</header>
         <h1>{props.title}</h1>
+        <p>{props.explanation}</p>   
+        <footer>{props.copyright}</footer>
       </div>
     );
   }
